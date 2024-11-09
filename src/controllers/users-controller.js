5db486363f36695a8f0f9fe1ea5f83ff4833ed4d
akbars-dev <akbarshox.coder@gmail.com>
2024-11-09 21:05:02 +0500

    Referall system created

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,24 +1,46 @@
 const prisma = require('../utils/prisma');
+const time = require('../utils/time');
 
 class UsersController {
     async register(req, res, next) {
         try {
-            const {firstName, profileImage, telegramId, referall} = req.body;
-            const candidate = await prisma.user.findFirst({where: { telegramId: telegramId.toString() }});
-
+            const { firstName, profileImage, telegramId, referall } = req.body;
+            const candidate = await prisma.user.findFirst({ where: { telegramId: telegramId.toString() } });
 
             if (candidate) return res.json({ message: "This user already exists", data: candidate });
+            else {
+                if (referall) {
+                    const reffer = await prisma.user.findFirst({ where: { telegramId: referall.toString() } })
+                    if (reffer) {
+                        await prisma.user.update({ where: { telegramId: referall }, data: { balance: reffer.balance + 100 } })
+                    }
+                }
 
-            const user = await prisma.user.create({ data: {
-                    firstName: firstName,
-                    profileImage: profileImage,
-                    telegramId: telegramId.toString(),
-                    referall: referall.toString()
-            } });
+                const user = await prisma.user.create({
+                    data: {
+                        firstName: firstName,
+                        profileImage: profileImage,
+                        telegramId: telegramId.toString(),
+                        referall: referall.toString(),
+                        date: time(new Date().toString()).formattedDate,
+                        time: time(new Date().toString()).formattedTime
+                    }
+                });
+
+                return res.json({ message: "User created", data: user });
+            }
+
+        } catch (error) {
+            next(error);
+        }
+    }
 
 
-            return res.json({ message: "User created", data: user });
-        }catch (error) {
+    async friends(req, res, next) {
+        try {
+            const friends = await prisma.user.findMany({ where: { referall: req.params.id.toString() } })
+            return res.json({ message: "Friends", data: friends });
+        } catch (error) {
             next(error);
         }
     }
--- src/routes/users-router.js
+++ src/routes/users-router.js
@@ -1,9 +1,9 @@
 const {Router } = require('express');
-const { register } = require('../controllers/users-controller')
+const { register, friends } = require('../controllers/users-controller')
 
 const userRouter = Router();
 
 userRouter.post('/register', register);
-
+userRouter.get('/friends/:id', friends)
 
 module.exports = userRouter;
