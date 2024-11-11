d0ef130daf58fdcd11764af508fc581d60621629
akbars-dev <akbarshox.coder@gmail.com>
2024-11-11 15:41:53 +0500

    Cors modifed

--- src/app.js
+++ src/app.js
@@ -6,10 +6,26 @@ const router = require('./routes');
 
 const app = express();
 
+const corsOptions = {
+    origin: '*', 
+    methods: 'GET,POST,DELETE,PATCH,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization,X-Requested-With',
+};
+
+app.use(cors(corsOptions));
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.json());
-app.use(cors(`Access-Control-Allow-Origin: *
-Access-Control-Allow-Methods: GET, POST, DELETE, PATCH, OPTIONS
-Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With`))
 app.use('/api', router);
 
 module.exports = app;
