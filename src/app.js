c439a17968dbff24c687404757e00edfc990164a
akbars-dev <akbarshox.coder@gmail.com>
2024-11-11 15:40:22 +0500

    Cors modifed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const router = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors("*"))
+app.use(cors(`Access-Control-Allow-Origin: *
+Access-Control-Allow-Methods: GET, POST, DELETE, PATCH, OPTIONS
+Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With`))
 app.use('/api', router);
 
 module.exports = app;
